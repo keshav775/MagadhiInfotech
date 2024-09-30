c6a031ebc2f476ebb0849436244b7e6ffb7ec9c4
KESHAV NARAYAN <keshavnarayan75@gmail.com>
2024-09-30 07:56:06 +0530

    upadte

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+    // Typed.js and AOS Init
+    var typed = new Typed('#element', {
+        strings: ['C++', 'JAVA', 'WEB DESIGN', 'UI/UX DESIGN', 'WEB DEV'],
+        typeSpeed: 100,
+    });
+
+    AOS.init({
+        offset: 100,
+        duration: 2000,
+    });
